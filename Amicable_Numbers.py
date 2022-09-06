75ea6abbe2a9faf683d633226b13478d0783bf05
KOLLEPARA RATHNA SRILEKHA <83913531+srilekha18@users.noreply.github.com>
2022-09-06 15:40:06 +0530

    n=int(input()) m=int(input()) pfs_n=0 for i in range(1,n):     if n%i==0:         pfs_n+=i pfs_m=0 for i in range(1,m):     if m%i==0:         pfs_m+=i
    if pfs_n==m and pfs_m==n:
        print("Amicable")
    else:
        print("Not Amicable")

---
+++ Amicable_Numbers.py
@@ -0,0 +1,17 @@
+n=int(input())
+m=int(input())
+pfs_n=0
+for i in range(1,n):
+    if n%i==0:
+        pfs_n+=i
+pfs_m=0
+for i in range(1,m):
+    if m%i==0:
+        pfs_m+=i
+
+if pfs_n==m and pfs_m==n:
+    print("Amicable")
+else:
+    print("Not Amicable")
+
+    
